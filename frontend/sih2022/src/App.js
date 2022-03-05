b650d7444f379c8e744773393c96ff31694cddc1
Aashutosh <ashutoshnishad2003@gmail.com>
2022-03-05 12:50:59 -0800

    Aadding Profile page NavBar Login-singup page

--- frontend/sih2022/src/App.js
+++ frontend/sih2022/src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
-import "./components/Login-Singup/Login.js";
-import "./components/StudenPage/StudentPage.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
-import Login from "./components/Login-Singup/Login.js";
-import InfoContenor from "./components/InfoContener/InfoContenor";
+import Home from "./components/home/Home";
+import Studentinfo from "./components/StudentPage/Studentinfo";
+import Totalstudent from "./components/StudentPage/Totalstudent";
+import Profile from "./components/StudentPage/Profile";
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<h1>Home page</h1>} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app">
+    <Navbar />
+      <Routes>
+       
+        <Route exact path="/" element={<><Home/> </> } />
+        <Route exact path="/students" element={<><Totalstudent/> </> } />
+        <Route exact path="/Profile" element={<><Profile/> </> } />
+      </Routes>
+    </div>
+  </Router>
+
   );
 }
 
--- frontend/sih2022/src/components/navbar/Navbar.js
+++ frontend/sih2022/src/components/navbar/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from "@mui/material";
 import React from "react";
 import './NavBar.css'
 
@@ -5,13 +6,13 @@ export default function Navbar() {
   return (
     <>
      <nav>
-        <div className="logo"> <img className="logo-image" src="./ks1-.jpg" alt=""/>
+        <div className="logo"> 
             <p className="logo-firstname">Student </p>
             <p className="logo-lastname">Tracker</p>
         </div>
         <ul className="navigation">
             <li id="homebutton" className="nav-link">Home</li>
-            <li id="skillbutton" className="nav-link">About Us</li>
+        <Link to="/Profile" >    <li id="skillbutton" className="nav-link">About Us</li> </Link>
             <li id="projectbutton" className="nav-link">FeedBack</li>
             <li id="contactbutton" className="nav-link">Log in </li>
         </ul>
